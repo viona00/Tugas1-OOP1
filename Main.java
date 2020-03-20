2b5b39a02a3b27c95697f1fcb875cdbda31a53b0
viona00 <62339493+viona00@users.noreply.github.com>
2020-03-20 20:38:53 +0700

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //Membuat Objek
+        Mahasiswa mhs = new Mahasiswa();
+
+        //Memberi Nilai/Value
+        mhs.nama = "Viona Paramitha";
+        mhs.kelas = "4B";
+        mhs.nim = "18090097";
+
+        //Memanggil Data
+        System.out.println("Nama: "+mhs.nama);
+        System.out.println("Kelas: "+mhs.kelas);
+        System.out.println("NIM: "+mhs.nim);
+        
+   
+    }
+}
